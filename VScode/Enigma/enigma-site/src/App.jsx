daca26e3e84b304e0f99e7baba2c61bca3f93670
thedarklord176 <donahueb85@gmail.com>
2022-12-06 17:05:48 -0600

    fixed init setup

---
+++ VScode/Enigma/enigma-site/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code className='text-5xl text-slate-700'>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;

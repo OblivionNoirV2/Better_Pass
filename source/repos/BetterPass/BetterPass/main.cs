6853b5a9b650515c4d6a88d7296c8dc70e8f7084
OblivionNoirV2 <donahueb85@gmail.com>
2023-01-16 12:09:14 -0600

    Fixed environment variable issues

---
+++ source/repos/BetterPass/BetterPass/main.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+
+class GetWord
+{
+    static async Task Main()
+    {
+
+        var client = new HttpClient();
+        string api_key = Environment.GetEnvironmentVariable("API_KEY_BP");
+        client.DefaultRequestHeaders.Add("X-Api-Key", api_key);
+        var response = await client.GetAsync("https://api.api-ninjas.com/v1/randomword");
+
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+        }
+        else
+        {
+            Console.WriteLine($"Error: {(int)response.StatusCode} {response.ReasonPhrase}");
+        };
+    }
+};

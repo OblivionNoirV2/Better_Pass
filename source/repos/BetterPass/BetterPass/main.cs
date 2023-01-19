557fc32d7cf1b946841e9b911031519fef8553c7
OblivionNoirV2 <donahueb85@gmail.com>
2023-01-19 16:49:10 -0600

    Set up user input

--- source/repos/BetterPass/BetterPass/main.cs
+++ source/repos/BetterPass/BetterPass/main.cs
@@ -1,26 +1,50 @@
-﻿
+﻿using System;
+//this enables async
 using System.Threading.Tasks;
-using System;
 using System.Net.Http;
 
-class GetWord
-{
-    static async Task Main()
-    {
 
-        var client = new HttpClient();
-        string api_key = Environment.GetEnvironmentVariable("API_KEY_BP");
-        client.DefaultRequestHeaders.Add("X-Api-Key", api_key);
-        var response = await client.GetAsync("https://api.api-ninjas.com/v1/randomword");
-
-        if (response.IsSuccessStatusCode)
+static class GetComplexity {
+    //this will only be called once per program cycle, so make it static
+    static void Main() {
+        Console.WriteLine(
+            "Select a complexity level, 1-3. 1 will meet requirements of most sites."
+            );
+        //take in the readline value, check for int...
+        while (!int.TryParse(Console.ReadLine(),
+            //name it
+            out int difficulty_selection)
+            //then ensure it fits the range
+            || difficulty_selection < 1
+            || difficulty_selection > 3)
         {
-            var result = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(result);
-        }
-        else
-        {
-            Console.WriteLine($"Error: {(int)response.StatusCode} {response.ReasonPhrase}");
+            Console.WriteLine("Please enter a valid response");
         };
     }
+};
+
+
+//this should be kept accessible so multiple words can be used for more complex passwords
+namespace WordSpace {
+    public class GetWord {
+  
+        static async Task WordFetch(){
+
+            var client = new HttpClient();
+            //Fetch the API key from my local machine
+            string api_key = Environment.GetEnvironmentVariable("API_KEY_BP");
+            client.DefaultRequestHeaders.Add("X-Api-Key", api_key);
+            var response = await client.GetAsync("https://api.api-ninjas.com/v1/randomword");
+
+            if (response.IsSuccessStatusCode) {
+                //wait for the async call to finish, then return response
+                var result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(result);
+
+            } else {
+
+                Console.WriteLine($"Error: {(int)response.StatusCode} {response.ReasonPhrase}");
+            };
+        }
+    };
 };

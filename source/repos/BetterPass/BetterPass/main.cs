45e04ca88878661c79dd3749a6209a2a0cd49f06
OblivionNoirV2 <donahueb85@gmail.com>
2023-01-20 16:52:23 -0600

    Skeleton of choice system set up

--- source/repos/BetterPass/BetterPass/main.cs
+++ source/repos/BetterPass/BetterPass/main.cs
@@ -23,20 +23,86 @@ public class GetComplexity
             Console.WriteLine("Please enter a valid response");
         };
         //enable moving to switchyswitch
-        SwitchySwitch switchy = new SwitchySwitch();
-        switchy.ChoosePath();
+        RandomAssembly choice = new RandomAssembly();
+        choice.RA();
+    }
+};
+//determine if user wants randomized password assembly
+public class RandomAssembly
+{
+    public bool RA()
+    {
+        bool WantsRandom;
+
+        Console.WriteLine(
+            "Do you want the order of the password components to be randomized? Type 'y' or 'n'"
+            );
+        while (true)
+        {
+            //catch any non-char responses
+            try
+            {
+                ConsoleKeyInfo response = Console.ReadKey();
+                if (response.KeyChar == 'y')
+                {
+                    HowMany Next = new HowMany();
+                    Next.HM();
+
+
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+};
+//determine how many passwords should be generated
+public class HowMany
+{
+    public static int HowManyP;
+    public int HM()
+    {
+        Console.WriteLine(
+            "How many generations should it make? Limit of 100"
+            );
+        while (!int.TryParse(Console.ReadLine(),
+            out HowManyP)
+            || HowManyP < 100
+            || HowManyP != 0)
+        {
+            Console.WriteLine("Please enter a value between 1 and 100");
+        };
+        Console.WriteLine(HowManyP);
+        return HowManyP;
     }
 };
 //determine the generation path
-public class SwitchySwitch
+//takes in 1 - 4 
+//if characters are not long enough with the words, append random letters from the alphabet 
+//if too long, remove the excess chars
+//1 = 8 characters, 1 uppercase, rest lowercase, 1 int, 1 special char
+//2 = 10 characters, 2 uppercase, rest lower, 2 int, 2 special char
+//3 = 2 words, 12 char, 3 uppercase, rest lower, 3 int, 3 special char 
+//4 2 words, 14 char, 4 uppercase, rest lower, 4 int, 4 special char
+
+//if "randomly assembled" is chosen, all of these elements get randomly
+//strung together instead of a simple append
+
+public class ChoosePath
 {
-    public void ChoosePath()
+    public void SwitchySwitch(int level, bool RandomAssembly)
     {
         //Console.WriteLine(GetComplexity.difficulty_selection + $" MADE IT!");
-        switch (GetComplexity.difficulty_selection)
+
+        switch (level)
         {
             case 1:
-
+                if (RandomAssembly)
+                {
+                    //move on to the generation algorithms with the data we now have
+                }
                 break;
 
             case 2:
@@ -60,7 +126,7 @@ public class SwitchySwitch
     }
 };
 
-//this should be kept accessible so multiple words can be used for more complex passwords
+
 namespace WordSpace 
 {
     public class GetWord 

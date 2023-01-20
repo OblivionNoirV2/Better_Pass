113157707f5801a678ac0ded990bbb715778824f
OblivionNoirV2 <donahueb85@gmail.com>
2023-01-19 20:24:51 -0600

    setup

--- source/repos/BetterPass/BetterPass/main.cs
+++ source/repos/BetterPass/BetterPass/main.cs
@@ -4,31 +4,70 @@ using System.Threading.Tasks;
 using System.Net.Http;
 
 
-static class GetComplexity {
-    //this will only be called once per program cycle, so make it static
-    static void Main() {
+public class GetComplexity 
+{
+    public static int difficulty_selection;
+    public static void Main() 
+    {
         Console.WriteLine(
-            "Select a complexity level, 1-3. 1 will meet requirements of most sites."
+            "Select a complexity level, 1-4. 1 will meet requirements of most sites."
             );
         //take in the readline value, check for int...
         while (!int.TryParse(Console.ReadLine(),
-            //name it
-            out int difficulty_selection)
+            out difficulty_selection)
             //then ensure it fits the range
             || difficulty_selection < 1
-            || difficulty_selection > 3)
+            || difficulty_selection > 4) 
         {
+
             Console.WriteLine("Please enter a valid response");
         };
+        //enable moving to switchyswitch
+        SwitchySwitch switchy = new SwitchySwitch();
+        switchy.ChoosePath();
+    }
+};
+//determine the generation path
+public class SwitchySwitch
+{
+    public void ChoosePath()
+    {
+        //Console.WriteLine(GetComplexity.difficulty_selection + $" MADE IT!");
+        switch (GetComplexity.difficulty_selection)
+        {
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            default:
+                Console.WriteLine("shits fucked");
+                break;
+
+        };
+
     }
 };
 
-
 //this should be kept accessible so multiple words can be used for more complex passwords
-namespace WordSpace {
-    public class GetWord {
+namespace WordSpace 
+{
+    public class GetWord 
+    {
   
-        static async Task WordFetch(){
+        static async Task WordFetch()
+        {
 
             var client = new HttpClient();
             //Fetch the API key from my local machine
@@ -36,12 +75,14 @@ namespace WordSpace {
             client.DefaultRequestHeaders.Add("X-Api-Key", api_key);
             var response = await client.GetAsync("https://api.api-ninjas.com/v1/randomword");
 
-            if (response.IsSuccessStatusCode) {
+            if (response.IsSuccessStatusCode) 
+            {
                 //wait for the async call to finish, then return response
                 var result = await response.Content.ReadAsStringAsync();
                 Console.WriteLine(result);
 
-            } else {
+            } else 
+            {
 
                 Console.WriteLine($"Error: {(int)response.StatusCode} {response.ReasonPhrase}");
             };

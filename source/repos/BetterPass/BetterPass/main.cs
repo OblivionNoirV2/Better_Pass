e2cdd60080025ae3f9907c4150a359c37c6cc10e
OblivionNoirV2 <donahueb85@gmail.com>
2023-01-26 21:54:01 -0600

    Nearly done!

--- source/repos/BetterPass/BetterPass/main.cs
+++ source/repos/BetterPass/BetterPass/main.cs
@@ -58,57 +58,35 @@ public class RandomAssembly
             WantsRandom = false;
         }
 
-        HowMany count = new HowMany();
-        count.HM(WantsRandom);
-
-    }
-};
-//determine how many passwords should be generated
-public class HowMany
-{
-    public int HowManyP;
-    public void HM(bool WantsRandom)
-    {
-        Console.WriteLine(
-            "How many generations should it make? Limit of 10"
-            );
-        while (!int.TryParse(Console.ReadLine(),
-            out HowManyP)
-            || HowManyP > 10
-            || HowManyP < 0)
-        {
-            Console.WriteLine("Please enter a value between 1 and 10");
-        };
-        
-        Console.WriteLine($"Creating {HowManyP} {(HowManyP == 1 ? "password": "passwords")}...");
+        Console.WriteLine("Creating...");
 
         ChoosePath path_ = new ChoosePath();
         //discard breaks the need for async
-        path_.FuncLoop(HowManyP, GetComplexity.difficulty_selection, WantsRandom);
+        path_.FuncLoop(GetComplexity.difficulty_selection, WantsRandom);
+
     }
 };
-
 
 //Used for symbols and any additional letters to meet the complexity requirements
 namespace Fluff
 {
     public class FluffChild
     {   //random numbers to append onto the word(s), each 1 digit
+        static readonly Random rnd_num = new Random();
         public static List<int> RandNums(int num_int_generations)
         {
             List<int> generated_nums = new List<int>();
-            Random rnd_num = new Random();
-            //int num = rnd_num.Next(1, 10);
+            
             for (int i = 0; i < num_int_generations; i++) {
                 //limit generations to 1 digit
                 int num = rnd_num.Next(1, 10);
                 generated_nums.Add(num);
-                Console.WriteLine(num);
             };
             //return the list from the loop
             return generated_nums;
         
         }
+        static readonly Random rnd_alph = new Random();
         public static List<char> RandAlph(int num_alph_generations)
         { 
             List<char> generated_alphs = new List<char>();
@@ -120,7 +98,7 @@ namespace Fluff
                 'p', 'q', 'r', 's', 't',
                 'u', 'v', 'w', 'x', 'y', 'z' 
             };
-            Random rnd_alph = new Random();
+            
            
             for (int i = 0; i < num_alph_generations; i++) { 
                 //get random value from alphabet
@@ -131,6 +109,7 @@ namespace Fluff
             };
             return generated_alphs;
         }
+        static readonly Random rnd_sym = new Random();
         public static List<char> RandSym(int num_sym_generations)
         {
             List<char> generated_syms = new List<char>();
@@ -141,13 +120,13 @@ namespace Fluff
                 '}', ']', '|', ':', ';', '"', '<' , '>', '.',
                 '?', '/'
             };
-            Random rnd_sym = new Random();
+            
             for (int i = 0; i < num_sym_generations; i++)
             {
                 int sym_index = rnd_sym.Next(0, symbols.Length);
                 char sym_char = symbols[sym_index];
                 generated_syms.Add(sym_char);
-                Console.WriteLine(sym_char);
+                
             }
             return generated_syms;
         }
@@ -160,13 +139,14 @@ public class ChoosePath
 
     public string MakeReadable()
     {
+        ChoosePath path1 = new ChoosePath();
         GetWord word_ = new GetWord();
         //generate initial word, then do a second if the complexity requires it
         string fetched_word = word_.WordFetch().Result;
         //First check that it's at least 4 characters for quality assurance purposes 
         while (fetched_word.Length < 4)
         {
-            MakeReadable();
+            path1.MakeReadable();
         }
         //convert to readable format
         JObject jsonObject = JObject.Parse(fetched_word);
@@ -175,32 +155,101 @@ public class ChoosePath
         return converted_word;
 
     }
-    public void FuncLoop(int count, int complexity, bool isRandom)
+    public void FuncLoop(int complexity, bool isRandom)
     {
-
-       // random doesn't need its own case. A couple simple ifs will suffice
-        bool is_complete = false;
+        
         ChoosePath path = new ChoosePath();
         string converted_word = path.MakeReadable();
-        string final_gen = null;
-        for (int i = 0; i < count; i++)
+        List <string>final_gens = new List<string>();
+        List<string> pass_components = new List<string>();
+
+         //set up words
+         converted_word = converted_word.Substring(0, converted_word.Length - 1) + 
+             char.ToUpper(converted_word[converted_word.Length - 1]);
+
+         pass_components.Add(converted_word);
+         
+         if (complexity > 2)
+         {
+             string second_word = path.MakeReadable();
+             second_word = second_word.Substring(0, second_word.Length - 1) + 
+                 char.ToUpper(second_word[second_word.Length - 1]); 
+
+             pass_components.Add(second_word);
+                    
+         }
+         //set up numbers 
+
+         List<int> generated_nums2 = new List<int>();
+                    
+         switch(complexity)
+         {
+             case 1:
+                 generated_nums2 = FluffChild.RandNums(1);
+             break;
+                 case 2:
+                 generated_nums2 = FluffChild.RandNums(2);
+             break;
+                 case 3:
+                 generated_nums2 = FluffChild.RandNums(3);
+             break;
+                 case 4:
+                 generated_nums2 = FluffChild.RandNums(4);
+             break;
+         }
+         //then convert to strings and append to pass_components
+         List<string> num2str = generated_nums2.ConvertAll(x => (string)(x + ""));
+         foreach (string str in num2str)
+         {
+             pass_components.Add(str);
+         }
+
+                //set up symbols
+
+         List<char> generated_syms2 = new List<char>();
+
+         switch (complexity)
+         {
+             case 1: 
+                 generated_syms2 = FluffChild.RandSym(1);
+             break;
+                 case 2:
+                 generated_syms2 = FluffChild.RandSym(2);
+             break;
+                 case 3:
+                 generated_syms2 = FluffChild.RandSym(3);
+             break;
+                 case 4:
+                 generated_syms2 = FluffChild.RandSym(4);
+             break;
+         }
+         //then convert to strings and append to pass_components
+         List<string> char2str = generated_syms2.ConvertAll(y => (string)(y + ""));
+         foreach(string str2 in char2str)
+         {
+             pass_components.Add(str2);
+         }
+         //Check for minimum lengths, add filler (pick randomly from the 3) as needed
+
+         //then concatonate, randomized if that was chosen
+
+         //then print it out and we're done! 
+
+         foreach(string z in pass_components)
+         {
+             Console.WriteLine(z);
+
+         }
+        Console.WriteLine("Again? (y if yes)");
+        string restart = Console.ReadLine();
+        if (restart == "y")
         {
-        
-           while(is_complete == false)
-            {
-                Console.WriteLine(converted_word);
-                //make first or last letter uppercase
-                //todo make it 50/50 chance for either
-                converted_word = char.ToUpper(converted_word[0]) + converted_word.Substring(1);
-                Console.WriteLine(converted_word);
-
-
-                is_complete = true;
-
-            }
-     
+            GetComplexity.Main();
         }
-           
+        else
+        {
+            Environment.Exit(0);
+        }   
     }
 };
 
@@ -213,25 +262,36 @@ public class GetWord
     {
 
         var client = new HttpClient();
+        //Timeout after 10 seconds
+        client.Timeout = TimeSpan.FromSeconds(10);
         //Fetch the API key from my local machine
         string api_key = Environment.GetEnvironmentVariable("API_KEY_BP");
         client.DefaultRequestHeaders.Add("X-Api-Key", api_key);
-        var response = await client.GetAsync("https://api.api-ninjas.com/v1/randomword");
-
-        if (response.IsSuccessStatusCode)
+        try
         {
-            //wait for the async call to finish, then return response
-            
-            return await response.Content.ReadAsStringAsync();
+            var response = await client.GetAsync("https://api.api-ninjas.com/v1/randomword");
+            if (response.IsSuccessStatusCode)
+            {
+                //wait for the async call to finish, then return response
+
+                return await response.Content.ReadAsStringAsync();
+
+            }
+            else
+            {
+
+                Console.WriteLine($"Error: {(int)response.StatusCode} {response.ReasonPhrase}");
+                return null;
+            }
+
 
         }
-        else
+        catch (TaskCanceledException) 
         {
-
-            Console.WriteLine($"Error: {(int)response.StatusCode} {response.ReasonPhrase}");
+            Console.WriteLine("Sorry, the request times out");
             return null;
-        };
-       
+
+        }
 
     }
    
